7e479b14770c4acc0125d21806b4ca4dfa83bb6b
krishna gupta <krishna.gupta_cs22@gla.ac.in>
2025-06-09 11:32:13 +0530

    agin change

---
+++ Even.java
@@ -0,0 +1,12 @@
+public class Even {
+    public static void main(String[] args) {
+      System.out.print("even numbers");
+
+	for(int i=0;i<=100;i++){
+	 if(i%2==0){
+		System.out.print(i);}
+
+	}
+
+      }
+}
